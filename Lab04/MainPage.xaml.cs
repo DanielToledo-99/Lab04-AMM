a1fc7ac8c7048c2d73177a613b87b5c8aff11aed
Daniel Toleo <toledo992905@gmail.com>
2022-04-14 22:31:14 -0500

    Subiendo app

---
+++ Lab04/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab04
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Page1());
+            };
+
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TabbedPage1());
+            };
+
+
+            Item5.Clicked += async (sender, e) =>
+            {
+                var answer = await DisplayAlert("Questions?",
+                    "Would you like to play a game", "Yes", "No");
+                Debug.WriteLine("Answer: " + answer);
+            };
+
+        }
+    }
+}
---
+++ Lab04/Page1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lab04
+{
+    
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            btnClose.Clicked += async (sender, e) =>
+            {
+                await Navigation.PopAsync();
+            };
+        }
+        async void OnNextPageButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
